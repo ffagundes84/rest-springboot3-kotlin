c757bf46eefc0db6c481d29f16e13c42ed1be77a
ffagundes84 <ffagundes84@gmail.com>
2026-01-17 09:53:03 -0300

    implement math calcs, exceptions and improve project folders

--- RestApiSample/src/main/kotlin/br/com/ffagundes/restapisample/application/controller/MathController.kt
+++ RestApiSample/src/main/kotlin/br/com/ffagundes/restapisample/application/controller/MathController.kt
@@ -2,6 +2,9 @@ package br.com.ffagundes.restapisample.application.controller
 
 import br.com.ffagundes.restapisample.application.extensions.StringExtension.isNumber
 import br.com.ffagundes.restapisample.domain.MathSerivce
+import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatusCode
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -9,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class MathController {
     val mathService = MathSerivce()
+    //@RequestMapping(value = ["sum/{numberOne}/{numberTwo}"])
     @RequestMapping(value = ["sum/{numberOne}/{numberTwo}"])
     fun sum(
         @PathVariable("numberOne") numberOne: String,
@@ -27,5 +31,10 @@ class MathController {
         if (!numberOne.isNumber() || !numberTwo.isNumber())
             throw UnsupportedOperationException("param is not a number")
         return mathService.subtraction(numberOne.toDouble(), numberTwo.toDouble())
+    }
+
+    @RequestMapping("/statusCodeOk")
+    fun statusCodeOk() : ResponseEntity<Int> {
+        return ResponseEntity<Int>(1, HttpStatus.OK)
     }
 }
